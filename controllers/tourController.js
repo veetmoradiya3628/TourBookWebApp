aed386412c0ddd89b0014698a020922ac5c640eb
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2023-03-05 00:16:14 +0530

    advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,14 +6,36 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = {...req.query};
+    // 1A. Filtering
+    const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el])
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, ' ', queryObj);
+    // 1B. Advaned filtering
+    // {difficulty : 'easy', duration: {$gte : 5}}
+    // gt, gte, lt, lte
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     // const tours = await Tour.find(queryObj);
-    const query = Tour.find(queryObj);
+    let query = Tour.find(JSON.parse(queryStr));
+  
+    // 2) Sorting
+    if(req.query.sort){
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy)
+    }else{
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Field limiting
+    if(req.query.fields){
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    }else{
+      query = query.select('-__v');
+    }
+
     const tours = await query;
 
     res.status(200).json({
