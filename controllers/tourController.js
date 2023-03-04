e2768211513656f3776e46ab8f5681c3529175bd
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2023-03-05 00:25:51 +0530

    pagination added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,23 +19,36 @@ exports.getAllTours = async (req, res) => {
 
     // const tours = await Tour.find(queryObj);
     let query = Tour.find(JSON.parse(queryStr));
-  
+
     // 2) Sorting
-    if(req.query.sort){
+    if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy)
-    }else{
+      query = query.sort(sortBy);
+    } else {
       query = query.sort('-createdAt');
     }
 
     // 3) Field limiting
-    if(req.query.fields){
+    if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
-    }else{
+    } else {
       query = query.select('-__v');
     }
 
+    // 4) pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=2&limit=10 -> 1-10,11-20,21-30,31-40...
+    query = query.skip(skip).limit(limit);
+
+    if(req.query.page){
+      const numTours = await Tour.countDocuments();
+      if(skip >= numTours) throw new Error('This page does not exists!!')
+    }
+
+    // Execute query
     const tours = await query;
 
     res.status(200).json({
