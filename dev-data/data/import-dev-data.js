1b19e7a97814b9d9caead741026ab0950a65a071
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2023-03-04 23:27:48 +0530

    simple filter added

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((_) => {
+    console.log('DB connection successful!!');
+  });
+
+//   READ JSON File
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+// IMPORT Data into database
+const importData = async () => {
+    try{
+        await Tour.create(tours);
+        console.log('Data successfully added!!');  
+    }catch(err){
+        console.log(err);
+    }
+    process.exit();
+}
+
+// Delete all data from collection
+const deleteData = async () => {
+    try{
+        await Tour.deleteMany();
+        console.log('Data successfully deleted!!');  
+    }catch(err){
+        console.log(err);
+    }
+    process.exit();
+}
+
+if(process.argv[2] === '--import'){
+    importData();
+}else if(process.argv[2] === '--delete'){
+    deleteData();
+}
