8458d7e04c7423766038fbbbba5fcc664f1a5f50
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2023-03-05 23:11:41 +0530

    Error segrigation added

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,37 @@
-module.exports = (err, req, res, next) => {
-    console.log(err.stack);
-  
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-  
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProduction = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+    // programming or other unknown error: don't leak error details
+  } else {
+    console.error('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!!',
+    });
   }
+};
+
+module.exports = (err, req, res, next) => {
+  console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(err, res);
+  }
+};
