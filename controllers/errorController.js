d09a7d7ef19727f34e56599c8b7e5ea163e91276
veetmoradiya3628 <veetmoradiya3628@gmail.com>
2023-03-06 00:12:47 +0530

    exception and error section ended

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,23 @@
 const AppError = require('./../utils/appError');
 
-const handleCastErrorDB = (err) => {
+const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
+
+const handleDuplicateFieldsDB = err => {
+    
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value);
+    const message = `Duplicate field value : ${value}. please use another value`;
+    return new AppError(message, 400);
+}
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+}
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -41,6 +55,8 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if(error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if(error.name === 'ValidationError') error = handleValidationErrorDB(error);
     sendErrorProduction(error, res);
   }
 };
